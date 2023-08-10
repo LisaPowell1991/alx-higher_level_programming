1e3270e3c3325e64b0494cc205d91e892bd4b068
LisaPowell1991 <lisapowell46@gmail.com>
2023-08-10 07:02:36 -0700

    prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+import sys
+
+def add_args():
+    total = 0
+
+    for argument in sys.argv[1:]:
+        total += int(argument)
+
+    print("{}".format(total))
+
+if __name__ == "__main__":
+    add_args()
