913e01a9ed285a227daa2f105678c78d627114a0
LisaPowell1991 <lisapowell46@gmail.com>
2023-08-10 07:01:08 -0700

    program that imports functions from the file and uses it

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+a = 10
+b = 5
+
+from calculator_1 import add, sub, mul, div
+sum_total = add(a, b)
+subtract_total = sub(a, b)
+multiply_total = mul(a, b)
+division_total = div(a, b)
+
+print("{} + {} = {}".format(a, b, sum_total))
+print("{} - {} = {}".format(a, b, subtract_total))
+print("{} x {} = {}".format(a, b, multiply_total))
+print("{} / {} = {}".format(a, b, division_total))
