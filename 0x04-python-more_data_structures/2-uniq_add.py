212c1911f7ce9a42766980a38e1256c30584373a
LisaPowell1991 <lisapowell46@gmail.com>
2023-08-16 05:27:24 -0700

    adds all unique integers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    new_list = set()
+    total = 0
+
+    for unique_num in my_list:
+        if unique_num not in new_list:
+            new_list.add(unique_num)
+            total += unique_num
+
+    return total
