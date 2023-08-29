b98174f59bb0a09a7eeb76b2b090f2bde0b6a072
LisaPowell1991 <lisapowell46@gmail.com>
2023-08-28 19:07:24 -0700

    prints an integer.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as value_error_exception:
+    except Exception as value_error_exception:
         print("Exception:", value_error_exception, file=sys.stderr)
         return False
