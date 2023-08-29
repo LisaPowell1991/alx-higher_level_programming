659f21edd344171f9e2454304e2db4a35dff36aa
LisaPowell1991 <lisapowell46@gmail.com>
2023-08-28 19:47:31 -0700

    Python bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+
+    for index in range(1, 3):
+        try:
+            if index > a:
+                raise Exception('Too far')
+        except ValueError:
+            pass
+        result = result + (a ** b) / index
+    result = result + a + b
+    return result
