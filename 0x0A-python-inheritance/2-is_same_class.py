a965bd563005b0153a27dd3a80d73a15b85b418a
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-11 06:32:58 -0700

    using type to check if obj is exactly an instance of a specific class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+""" The module contains a fuction, is_same_class """
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if a object is exactly an instance of the specified class
+
+    Args:
+    @obj: The object to be inspected
+    @a_class: The class to compare with
+
+    Returns:
+    bool: True if the object is exactly an instance of the specified class
+    otherwise False.
+    """
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
