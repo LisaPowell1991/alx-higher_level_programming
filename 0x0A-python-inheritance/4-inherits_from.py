a25d04834b5f4f64ea27391ef46a98a75ca9ae52
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-11 07:45:27 -0700

    using issubclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+""" This module contains a function, inherits_from """
+
+
+def inherits_from(obj, a_class):
+    """
+    checks if  object is an instance of a class that
+    inherited (directly or indirectly) from the specified class.
+
+    Args:
+    @obj: The object to be inspected
+    @a_class: The class to compare with
+
+    Returns:
+    Bool: True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False.
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
