8d747037d3664e786640887bd94e1fb3323b4363
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-11 01:57:22 -0700

    The list of available attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+
+def lookup(obj):
+    """
+    a function that returns the list of
+    available attributes and methods of an object
+
+    Args:
+    @obj: The object
+
+    Return:
+    A list object
+    """
+
+    attributes_and_methods = dir(obj)
+
+    return attributes_and_methods
