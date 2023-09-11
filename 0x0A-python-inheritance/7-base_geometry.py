d57362ebbe96a3d33f8bfb3ff11c3b9e191c733b
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-11 08:25:31 -0700

    creating 2 public instance and 2 raises

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+
+""" This module contains an empty class, BaseGeometry. """
+
+
+class BaseGeometry:
+    """
+    This class defines BaseGeometry class
+    """
+
+    def area(self):
+        """
+        A public instance method, called area, that calculates the area
+
+        Raises:
+        Exception is raised if subclass does not implement this method
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        A public instance method, called integer_validator that validates value
+
+        Args:
+        @name: The name of the value
+        @value: The value to be validated
+
+        Raises:
+        TypeError: If value is not an integer
+        ValueError: if value is <= 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
