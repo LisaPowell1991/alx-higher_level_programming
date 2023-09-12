79a544ac3b546380e779ed7695ac3f6b27950eef
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-12 05:15:42 -0700

    a function that returns an object (Python data structure) represented by a JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+""" A module containing a function, from_json_string """
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    A function that returns an object (string) represented by a JSON string
+
+    Args:
+    @my_str: Name of string that needs to be converted
+
+    Returns:
+    an object (Python data structure) represented by a JSON string
+    """
+
+    return (json.loads(my_str))
