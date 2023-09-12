6b161dfd5941a98e4976833959d3c71696237310
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-11 19:05:41 -0700

    prints a magic string, __str__

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+""" This module contains a subclass, Square that inherits from Rectangle """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Represent a Square class that inherits from Rectangle. """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+        @size (int): the sides of the square
+        """
+
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """ Calculates and returns the areaof the rectangle """
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ Return a string representation of the square """
+
+        return (f"[Square] {self.__size}/{self.__size}")
