271d67b418c7b77c151af46ce00c7c6e864af086
LisaPowell1991 <lisapowell46@gmail.com>
2023-09-17 22:15:27 -0700

    created a class, Rectangle that inherits from base and test cases

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+
+""" This module contains a class Rectangle """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ A class Rectangle that inherits from Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a Rectangle object
+
+        Args:
+        width (int): Width of rectangle
+        height (int): Height of rectangle
+        x (int, optional): The x-coordinate of the new Rectangle.
+        y (int, optional): The y-coordinate of the new Rectangle.
+        id (int, optional): Id of the new Rectangle
+
+        """
+
+        super().__init__(id)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Sets the width of the Rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("Width must be an integer")
+        if value <= 0:
+            raise ValueError("Width must be > 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Sets the height of the Rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("Height must be an integer")
+        if value <= 0:
+            raise ValueError("Height must be > 0")
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Sets the x coordinates of the Rectangle """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Sets the y coordinates of the Rectangle """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+""" This module defines unittest cases for Rectangle """
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ Test cases for the Rectangle class """
+
+    def test_rectangle_with_valid_values(self):
+        """
+        Test creating a Rectangle with valid
+        width, height, x and y values.
+        """
+
+        rectangle = Rectangle(10, 20, 2, 3, 1)
+        self.assertEqual(rectangle.width, 10)
+        self.assertEqual(rectangle.height, 20)
+        self.assertEqual(rectangle.x, 2)
+        self.assertEqual(rectangle.y, 3)
+        self.assertEqual(rectangle.id, 1)
+
+    def test_rectangle_with_negative_width(self):
+        """
+        Test creating a Rectangle with a negative width.
+        (Should raise ValueError)
+        """
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(-5, 20, 2, 3, 1)
+
+    def test_rectangle_with_negative_height(self):
+        """
+        Test creating a Rectangle with a negative height.
+        (Should raise ValueError)
+        """
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(10, -5, 2, 3, 1)
+
+    def test_rectangle_with_negative_x(self):
+        """
+        Test creating a Rectangle with a negative x.
+        (Should raise ValueError)
+        """
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(10, 5, -2, 20, 5)
+
+    def test_rectangle_with_negative_y(self):
+        """
+        Test creating a Rectangle with a negative y.
+        (Should raise ValueError)
+        """
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(10, 8, 4, -10, 1)
+
+    def test_rectangle_with_non_integer_width(self):
+        """
+        Test creating a Rectangle with a non-integer width
+        (should raise TypeError).
+        """
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle("invalid", 20, 3, 7, 9)
+
+    def test_rectangle_with_non_integer_height(self):
+        """
+        Test creating a Rectangle with a non-integer height
+        (should raise TypeError).
+        """
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(10, "invalid", 5, 1, 3)
+
+    def test_rectangle_with_non_integer_x(self):
+        """
+        Test creating a Rectangle with a non-integer x
+        (should raise TypeError).
+        """
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(13, 6, "invalid", 3, 1)
+
+    def test_rectangle_with_non_integer_y(self):
+        """
+        Test creating a Rectangle with a non-integer y
+        (should raise TypeError).
+        """
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(14, 8, 20, "invalid", 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
