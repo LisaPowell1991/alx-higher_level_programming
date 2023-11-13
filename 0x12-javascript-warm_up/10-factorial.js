1829b8774b75dc05d21a08c0d8782b0725a3e1bd
lisapowell1991 <lisapowell46@gmail.com>
2023-11-13 06:33:43 -0800

    computes and prints a factorial by creating a factorial function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+function calculateFactorial (n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * calculateFactorial(n - 1);
+  }
+}
+
+const firstArgument = process.argv[2];
+
+const factorialNum = parseInt(firstArgument);
+
+if (isNaN(factorialNum)) {
+  console.log('1');
+} else {
+  const result = calculateFactorial(factorialNum);
+  console.log(`${result}`);
+}
