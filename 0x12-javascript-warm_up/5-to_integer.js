88437bf904e6c98edf724dc78c320b935aa64c3a
lisapowell1991 <lisapowell46@gmail.com>
2023-11-13 04:56:58 -0800

    convert first arg to an int if possible using parseInt

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const firstArgument = process.argv[2];
+
+const parsedInteger = parseInt(firstArgument);
+
+if (isNaN(parsedInteger)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parsedInteger}`);
+}
