a937c1ce9520ad6ac25b04a572962a179e13997b
lisapowell1991 <lisapowell46@gmail.com>
2023-11-13 06:58:54 -0800

    searches the second biggest integer in the list of arguments. using map, sort, parseInt

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const argumentList = process.argv.slice(2);
+
+if (argumentList.length === 0 || argumentList.length === 1) {
+  console.log('0');
+} else {
+  const integers = argumentList.map(arg => parseInt(arg));
+
+  const sortedIntegers = integers.sort((a, b) => b - a);
+
+  const secondBiggest = sortedIntegers[1];
+
+  console.log(secondBiggest);
+}
