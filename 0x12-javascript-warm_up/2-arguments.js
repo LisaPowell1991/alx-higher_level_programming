85407495367b6293561b4d3a012136967e3e6f58
lisapowell1991 <lisapowell46@gmail.com>
2023-11-13 03:49:46 -0800

    using if, else if and else

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const numOfArgs = process.argv.length - 2;
+
+if (numOfArgs === 0) {
+  console.log('No arguments');
+} else if (numOfArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
