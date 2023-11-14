3ea49fea111761d4751c29ffc90831269a3d0aef
lisapowell1991 <lisapowell46@gmail.com>
2023-11-14 06:50:06 -0800

    prints the number of arguments already printed and the new argument value.

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+exports.logMe = function (item) {
+  if (!exports.logMe.count) {
+    exports.logMe.count = 0;
+  }
+  console.log(exports.logMe.count + ': ' + item);
+
+  exports.logMe.count++;
+};
