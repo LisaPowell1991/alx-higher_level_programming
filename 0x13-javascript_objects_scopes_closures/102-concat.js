abafca2482f7bd3ee7c95426f6b70087f1f665eb
lisapowell1991 <lisapowell46@gmail.com>
2023-11-14 19:17:04 -0800

    concat 2 files using fs.readFileSync and fs.writeFileSync

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const contentA = fs.readFileSync(process.argv[2], 'utf-8');
+const contentB = fs.readFileSync(process.argv[3], 'utf-8');
+
+const combinedContent = `${contentA}${contentB}`;
+
+fs.writeFileSync(process.argv[4], combinedContent);
