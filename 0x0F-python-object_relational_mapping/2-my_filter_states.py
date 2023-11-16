c10e1b5219d93fdabbc18f18d4485e1f854899ad
LisaPowell1991 <lisapowell46@gmail.com>
2023-11-16 05:06:53 -0800

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument using SELECT,FROM, WHERE, LIKE ORDER BY ASC

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+
+"""
+A script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+
+    )
+    cursor = db.cursor()
+
+    query = "SELECT * FROM
+    states WHERE name LIKE '{}' ORDER BY id ASC".format(state_name_searched)
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
