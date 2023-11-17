6ad4f1ce3b14827428f90709f2111f4d525d56f4
LisaPowell1991 <lisapowell46@gmail.com>
2023-11-17 02:17:20 -0800

    script that lists all State objects from the database hbtn_0e_6_usa using create_engine, metadata.bind, sessionmaker

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+"""
+A script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+            f'mysql://{username}:{password}@localhost:3306/{db}', echo=True)
+
+    Base.metadata.bind = engine
+
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
