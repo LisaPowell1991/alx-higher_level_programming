bb64acfd2895c302f31746ba8365160b0c38dc34
LisaPowell1991 <lisapowell46@gmail.com>
2023-11-17 02:41:48 -0800

    script that prints the first State object from the database hbtn_0e_6_usa using first() instead of all() by session.query

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+
+"""
+A script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+            f'mysql://{username}:{password}@localhost:3306/{db}')
+
+    Base.metadata.bind = engine
+
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
