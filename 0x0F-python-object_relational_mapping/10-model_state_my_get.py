0f1d62c326c356cabd8c7d1cb4832639ecd5fa8f
LisaPowell1991 <lisapowell46@gmail.com>
2023-11-17 03:07:27 -0800

    prints the State object with the name passed as argument using filter_by

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+"""
+A script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+            f'mysql://{username}:{password}@localhost:3306/{db}')
+
+    Base.metadata.bind = engine
+
+    DBsession = sessionmaker(bind=engine)
+    session = DBsession()
+
+    state_result = session.query(State).filter_by(name=state_name).first()
+
+    if state_result:
+        print(state_result.id)
+    else:
+        print("Not found")
+
+    session.close()
