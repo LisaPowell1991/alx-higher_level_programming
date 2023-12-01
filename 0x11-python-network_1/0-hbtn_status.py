e9b945d5ed7f513ff367abd5d09ece7aeeab9a9d
lisapowell1991 <lisapowell46@gmail.com>
2023-12-01 00:50:52 -0800

    script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,12 +6,12 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 from urllib import request
 
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
 
-with request.urlopen(url) as response:
-    body = response.read()
-
-    print("Body response:")
-    print("   - type", type(body))
-    print("   - content:", body)
-    print("   - utf8 content:", body.decode('utf-8'))
+    with request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"{'   - type:'} {type(body)}")
+        print(f"{'   - content:'} {body}")
+        print(f"{'   - utf8 content:'} {body.decode('utf-8')}")
