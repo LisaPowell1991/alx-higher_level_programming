ce6d402c579dbd4a959da9ea1f282aee21eaf1f2
lisapowell1991 <lisapowell46@gmail.com>
2023-12-01 00:12:53 -0800

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+from urllib import request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with request.urlopen(url) as response:
+    body = response.read()
+
+    print("Body response:")
+    print("   - type", type(body))
+    print("   - content:", body)
+    print("   - utf8 content:", body.decode('utf-8'))
