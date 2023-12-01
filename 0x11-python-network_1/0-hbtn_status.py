f0809d6bdcd7a9d06a291c9130508b1ad1337d4a
lisapowell1991 <lisapowell46@gmail.com>
2023-12-01 02:04:01 -0800

    script that fetches https://alx-intranet.hbtn.io/status using package requests

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print(f"{'   - type:'} {type(body)}")
-        print(f"{'   - content:'} {body}")
-        print(f"{'   - utf8 content:'} {body.decode('utf-8')}")
+        print(f"{'  - type:'} {type(body)}")
+        print(f"{'  - content:'} {body}")
+        print(f"{'  - utf8 content:'} {body.decode('utf-8')}")
