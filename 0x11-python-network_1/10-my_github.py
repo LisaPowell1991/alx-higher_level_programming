febe96517dbb940eec4c4efdcea46815179dc0f4
lisapowell1991 <lisapowell46@gmail.com>
2023-12-01 06:14:56 -0800

    a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+a Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+
+    api_url = "https://api.github.com/user"
+
+    auth = (username, access_token)
+
+    response = requests.get(api_url, auth=auth)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print(f"{user_data['id']}")
+    else:
+        print("None")
