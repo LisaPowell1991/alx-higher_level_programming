5b4169313c1af25b2f386ebacf7c548d2f6b8a16
lisapowell1991 <lisapowell46@gmail.com>
2024-01-03 03:41:48 -0800

    a script that display the status code of a GET request. by using the request module

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const urlToRequest = process.argv[2];
+
+request(urlToRequest, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(response.statusCode);
+  }
+});
