21ed1a0f528c248bd23c9afc1772f6f008e40f32
lisapowell1991 <lisapowell46@gmail.com>
2024-01-03 05:11:07 -0800

    a script that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const urlToRequest = process.argv[2];
+
+const filePath = process.argv[3];
+
+request(urlToRequest, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const fs = require('fs');
+    fs.writeFileSync(filePath, body, 'utf-8');
+  }
+});
