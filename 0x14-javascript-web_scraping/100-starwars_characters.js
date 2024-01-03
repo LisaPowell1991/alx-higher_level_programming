7eb03ccbf5ed450d6a8a1faaf47e932631134a48
lisapowell1991 <lisapowell46@gmail.com>
2024-01-03 07:06:39 -0800

    semistandard fixed

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,17 +4,23 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-request(`https://swapi.dev/api/films/${movieId}`, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
     const movieInfo = JSON.parse(body);
 
-    movieInfo.characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const characterInfo = JSON.parse(body);
+    for (const characterUrl of movieInfo.characters) {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error('Error fetching character details:', charError);
+        } else {
+          const characterInfo = JSON.parse(charBody);
           console.log(characterInfo.name);
         }
       });
-    });
+    }
   }
 });
