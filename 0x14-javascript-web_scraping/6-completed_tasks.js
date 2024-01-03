6e6f3cadd667dbd0438d8d1c22fcf451e76faef8
lisapowell1991 <lisapowell46@gmail.com>
2024-01-03 05:31:33 -0800

    a script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasksInfo = JSON.parse(body);
+
+    const userCompletedTasks = {};
+
+    tasksInfo.forEach(task => {
+      if (task.completed) {
+        if (!userCompletedTasks[task.userId]) {
+          userCompletedTasks[task.userId] = 1;
+        } else {
+          userCompletedTasks[task.userId]++;
+        }
+      }
+    });
+    console.log(JSON.stringify(userCompletedTasks, null, 2));
+  }
+});
