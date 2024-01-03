54a54588f88360a57b059b045c3b5d666c5e5bc1
lisapowell1991 <lisapowell46@gmail.com>
2024-01-03 03:46:22 -0800

    added missing 'code' before the status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,6 +8,6 @@ request(urlToRequest, (error, response) => {
   if (error) {
     console.error(error);
   } else {
-    console.log(response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
